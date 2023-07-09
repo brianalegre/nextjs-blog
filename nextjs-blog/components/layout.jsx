4d3af34b1fc48380ae71c97d25826548ef439f2a
Brian Alegre <brialegre@yahoo.com>
2023-07-09 15:38:49 -0700

    Created layout component

---
+++ nextjs-blog/components/layout.jsx
@@ -0,0 +1,8 @@
+// Import CSS
+import styles from './layout.module.css';
+
+export default function Layout({ children }) {
+    return (
+        <div className={styles.container}>{children}</div>
+    );
+};
--- nextjs-blog/pages/posts/first-post.jsx
+++ nextjs-blog/pages/posts/first-post.jsx
@@ -7,27 +7,31 @@ import Link from 'next/link';
 // Script - used for when using CDNs/SDKs
 import Script from 'next/script';
 
+// Import Layout Component from components/layout.jsx
+import Layout from '../../components/layout';
+
 // Export the main function of the page
 export default function FirstPost() {
     return (
-        <>
+        // Add Layout Component
+        <Layout>
             {/* Metadata */}
             <Head>
                 <title>First Post</title>
             </Head>
             {/* Script */}
-            <Script
-                src="https://connect.facebook.net/en_US/sdk.js"
-                strategy="lazyOnload"
-                onLoad={() =>
-                    console.log(`script loaded correctly, window.FB has been populated`)
-                }   
-            />
+            {/* <Script
+                    src="https://connect.facebook.net/en_US/sdk.js"
+                    strategy="lazyOnload"
+                    onLoad={() =>
+                        console.log(`script loaded correctly, window.FB has been populated`)
+                    }
+                /> */}
             <h1>First Post</h1>
             <h2>
                 {/* Link to the Home Page - pages/index.jsx */}
                 <Link href="/">Back to home</Link>
             </h2>
-        </>
+        </Layout>
     );
 }
