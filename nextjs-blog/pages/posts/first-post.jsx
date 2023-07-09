82810a9a3e23889b8f504357c66fbee2b4b5dec3
Brian Alegre <brialegre@yahoo.com>
2023-07-09 15:19:09 -0700

    Added notes/comments

--- nextjs-blog/pages/posts/first-post.jsx
+++ nextjs-blog/pages/posts/first-post.jsx
@@ -1,14 +1,20 @@
+// Head - used for updating meta data
 import Head from 'next/head';
+
+// Link - used for linking pages similar to <a>
 import Link from 'next/link';
 
+// Export the main function of the page
 export default function FirstPost() {
     return (
         <>
+            {/* Metadata */}
             <Head>
                 <title>First Post</title>
             </Head>
             <h1>First Post</h1>
             <h2>
+                {/* Link to the Home Page - pages/index.jsx */}
                 <Link href="/">Back to home</Link>
             </h2>
         </>
