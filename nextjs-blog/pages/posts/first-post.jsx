d002009e26b8f1f5effa3a88de711e0a4bede9d7
Brian Alegre <brialegre@yahoo.com>
2023-07-09 15:26:53 -0700

    Testing next/scripts component

--- nextjs-blog/pages/posts/first-post.jsx
+++ nextjs-blog/pages/posts/first-post.jsx
@@ -4,6 +4,9 @@ import Head from 'next/head';
 // Link - used for linking pages similar to <a>
 import Link from 'next/link';
 
+// Script - used for when using CDNs/SDKs
+import Script from 'next/script';
+
 // Export the main function of the page
 export default function FirstPost() {
     return (
@@ -12,6 +15,14 @@ export default function FirstPost() {
             <Head>
                 <title>First Post</title>
             </Head>
+            {/* Script */}
+            <Script
+                src="https://connect.facebook.net/en_US/sdk.js"
+                strategy="lazyOnload"
+                onLoad={() =>
+                    console.log(`script loaded correctly, window.FB has been populated`)
+                }   
+            />
             <h1>First Post</h1>
             <h2>
                 {/* Link to the Home Page - pages/index.jsx */}
