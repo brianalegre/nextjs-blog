96a51e35c0f1f2f3dffe933bdcf0712ff880c6e3
Brian Alegre <brialegre@yahoo.com>
2023-07-09 21:56:27 -0700

    Added getStaticProps to index.jsx

--- nextjs-blog/pages/index.jsx
+++ nextjs-blog/pages/index.jsx
@@ -9,7 +9,21 @@ import utilStyles from '../styles/utils.module.css'
 // Import Components
 import Layout, { siteTitle } from '../components/layout';
 
-export default function Home() {
+// Import Lib
+import { getSortedPostsData } from '../lib/posts';
+
+// getStaticProps
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+// Home 
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -21,6 +35,20 @@ export default function Home() {
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
