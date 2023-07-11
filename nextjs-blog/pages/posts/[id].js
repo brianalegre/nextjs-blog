7fe5adc244488c37eac82f91b10baa303e17342b
Brian Alegre <brialegre@yahoo.com>
2023-07-10 21:35:36 -0700

    Created dynamic routes

---
+++ nextjs-blog/pages/posts/[id].js
@@ -0,0 +1,35 @@
+// Import Components
+import Layout from '../../components/layout';
+
+// Import Lib
+import { getAllPostIds, getPostData } from '../../lib/posts';
+
+export async function getStaticProps({ params }) {
+    const postData = getPostData(params.id);
+    return {
+        props: {
+            postData,
+        },
+    };
+}
+
+export async function getStaticPaths() {
+    const paths = getAllPostIds();
+    return {
+        paths,
+        fallback: false,
+    };
+}
+
+
+export default function Post({ postData }) {
+    return (
+        <Layout>
+            {postData.title}
+            <br />
+            {postData.id}
+            <br />
+            {postData.date}
+        </Layout>
+    );
+}
